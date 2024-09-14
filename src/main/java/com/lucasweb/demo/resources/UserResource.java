439147daf1f0138e639070743a7eaf1b0c303ba4
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2024-09-14 18:19:46 -0300

    Exception handling - findById

--- src/main/java/com/lucasweb/demo/resources/UserResource.java
+++ src/main/java/com/lucasweb/demo/resources/UserResource.java
@@ -55,6 +55,6 @@ public class UserResource {
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<User> update(@PathVariable long id, @RequestBody User obj) {
 		obj = service.update(id, obj);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj);	
 	}
 }
