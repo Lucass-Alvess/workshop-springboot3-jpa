92c4f01a3066a7a4e5e9c447622a616e2c4b74ce
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2024-09-14 18:39:16 -0300

    Exception handling - delete

--- src/main/java/com/lucasweb/demo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/lucasweb/demo/resources/exception/ResourceExceptionHandler.java
@@ -7,21 +7,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lucasweb.demo.services.exception.DatabaseExeption;
 import com.lucasweb.demo.services.exception.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
+	@ExceptionHandler(DatabaseExeption.class)
+	public ResponseEntity<StandardError> database(DatabaseExeption e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 
 }
-	
--- src/main/java/com/lucasweb/demo/services/UserService.java
+++ src/main/java/com/lucasweb/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lucasweb.demo.entities.User;
 import com.lucasweb.demo.repositories.UserRepository;
+import com.lucasweb.demo.services.exception.DatabaseExeption;
 import com.lucasweb.demo.services.exception.ResourceNotFoundException;
 
 @Service
@@ -29,10 +32,17 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
-	public void delete(Long id) {
-		repository.deleteById(id);
+	public void delete(Long id){
+	    try {
+	        if(!repository.existsById(id)) throw new ResourceNotFoundException(id);
+	        repository.deleteById(id);
+	    }catch (ResourceNotFoundException e){
+	        throw new ResourceNotFoundException(id);
+	    } catch(DataIntegrityViolationException e) {
+	    	throw new DatabaseExeption(e.getMessage());
+	    }
 	}
-	
+	 
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
